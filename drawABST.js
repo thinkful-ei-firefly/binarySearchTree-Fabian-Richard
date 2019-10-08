c7853d430d815b5d7f8bd0561a4461830a626de1
rjgates3 <gates.j.richard@gmail.com>
2019-10-08 09:15:09 -0600

    question 1 and 2

---
+++ drawABST.js
@@ -0,0 +1,41 @@
+/*
+3, 1, 4, 6, 9, 2, 5, 7
+
+            3
+        1       4
+          2       6
+                5   9
+                   7
+Delet Root
+
+
+            2
+        1       4
+                  6
+                5   9
+                   7
+
+*/
+
+/*
+EASYQUESTION
+
+A<B<C<D....
+
+            E
+        A       S
+              Q      Y
+            I       U
+             O     T
+            N
+
+Delete Root
+
+
+            A
+                 S
+              Q      Y
+            I       U
+             O     T
+            N
+*/
