287d4a0bbf257d3596147b3ca6b0a50cbf6ce9f2
rjgates3 <gates.j.richard@gmail.com>
2019-10-08 15:11:51 -0600

    isSame function

---
+++ isSame.js
@@ -0,0 +1,17 @@
+let arr1 = [3, 5, 4, 6, 1, 0, 2]; // i
+let arr2 = [3, 1, 5, 2, 4, 6, 0]; // j
+
+
+const isSame = (arr1, arr2) => {
+    //base cases
+    if(arr1.length !== arr2.length || arr1.length === 0 || arr1[0] !== arr2[0]) {
+        return false;
+    }
+
+    let arr1Left = for(let i=1; i<arr1.length; i++) 
+    let arr1Right
+    let arr2Left 
+    let arr2Right
+    
+
+};
